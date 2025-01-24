39e73acf179d052eaa55dd550533814eb57e96a8
Adam Sawicki <@>
2025-01-25 00:53:39 +0100

    Fixed order of spawned players

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var player_spawners_node: Node2D = $PlayerSpawners
+
 # Elements will be removed from the beginning of this array.
 @onready var player_spawners = [
 	$PlayerSpawners/PlayerSpawner1, $PlayerSpawners/PlayerSpawner2,
@@ -10,7 +12,7 @@ var player_per_input_device_spawned := [false, false, false, false]
 
 func process_spawning_player(input_device: GlobalValues.INPUT_DEVICE) -> void:
 	if GlobalValues.IsInputAction1Pressed(input_device) and not player_per_input_device_spawned[input_device]:
-		player_spawners[0].spawn_player(input_device)
+		player_spawners[0].spawn_player(player_spawners_node, input_device)
 		player_per_input_device_spawned[input_device] = true
 		player_spawners[0].queue_free()
 		player_spawners.remove_at(0)
--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -6,11 +6,11 @@ const SIZE := 1.5
 @export var cloth_color_modulate := Color.WHITE
 @export var hair_color_modulate := Color.WHITE
 
-func spawn_player(input_device: GlobalValues.INPUT_DEVICE) -> void:
+func spawn_player(parent_node: Node, input_device: GlobalValues.INPUT_DEVICE) -> void:
 	var obj = PLAYER.instantiate()
 	obj.cloth_color_modulate = cloth_color_modulate
 	obj.hair_color_modulate = hair_color_modulate
 	obj.input_device = input_device
-	get_tree().root.get_child(1).add_child(obj)
+	parent_node.add_child(obj)
 	obj.position = position
 	obj.scale = Vector2(SIZE, SIZE)
