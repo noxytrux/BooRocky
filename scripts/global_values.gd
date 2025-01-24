5274ab1bc383a7b4f5ee453ef6971702718a1385
Adam Sawicki <@>
2025-01-24 19:04:37 +0100

    Finished implementing input for 4 players

--- scripts/global_values.gd
+++ scripts/global_values.gd
@@ -3,3 +3,5 @@ class_name GlobalValues extends Node
 enum TEST_ENUM { TEST_ENUM_ITEM1, TEST_ENUM_ITEM2 }
 
 const TEST_CONST := 42
+
+enum INPUT_DEVICE { WSAD, ARROWS, GAMEPAD1, GAMEPAD2 }
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,32 @@
 extends CharacterBody2D
 
 const SPEED := 500.0
+
+@export var input_device := GlobalValues.INPUT_DEVICE.WSAD
 @export var color := Color.RED
-@export var input_action_left := ""
-@export var input_action_right := ""
-@export var input_action_up := ""
-@export var input_action_down := ""
-@export var input_action_1 := ""
-@export var input_action_2 := ""
+
 @onready var polygon_2d: Polygon2D = $Polygon2D
 
 func _ready() -> void:
 	polygon_2d.color = color
 
 func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector(input_action_left, input_action_right, input_action_up, input_action_down)
+	var dir = Vector2.ZERO
+	match input_device:
+		GlobalValues.INPUT_DEVICE.WSAD:
+			dir = Input.get_vector("A", "D", "W", "S")
+		GlobalValues.INPUT_DEVICE.ARROWS:
+			dir = Input.get_vector("Left", "Right", "Up", "Down")
+		GlobalValues.INPUT_DEVICE.GAMEPAD1:
+			dir = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X),
+				Input.get_joy_axis(0, JOY_AXIS_LEFT_Y))
+		GlobalValues.INPUT_DEVICE.GAMEPAD2:
+			dir = Vector2(Input.get_joy_axis(1, JOY_AXIS_LEFT_X),
+				Input.get_joy_axis(1, JOY_AXIS_LEFT_Y))
+	# Introduce dead zone.
+	if dir.length_squared() > 0.1:
+		dir = dir.normalized()
+	else:
+		dir = Vector2.ZERO
 	velocity = dir * SPEED
 	move_and_slide()
