dfcaa305c47c69742833a8dd8345441243962bdc
Adam Sawicki <@>
2025-01-24 18:36:05 +0100

    Implemented 2 players having separate inputs

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
 const SPEED := 500.0
+@export var color := Color.RED
+@export var input_action_left := ""
+@export var input_action_right := ""
+@export var input_action_up := ""
+@export var input_action_down := ""
+@export var input_action_1 := ""
+@export var input_action_2 := ""
+@onready var polygon_2d: Polygon2D = $Polygon2D
 
 func _ready() -> void:
-	pass
+	polygon_2d.color = color
 
 func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector("Left", "Right", "Up", "Down")
+	var dir = Input.get_vector(input_action_left, input_action_right, input_action_up, input_action_down)
 	velocity = dir * SPEED
 	move_and_slide()
