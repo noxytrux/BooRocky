234f96a901202ecb5aa831b5152892a21db9aa74
Adam Sawicki <@>
2025-01-24 17:52:27 +0100

    Started coding player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,13 @@
-extends Node2D
+extends CharacterBody2D
+
+const SPEED := 3000.0
 
 func _ready() -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	var dir = Input.get_vector("Left", "Right", "Down", "Up")
+	print("dir=%s" % dir)
+	#var xformed_dir = transform.basis_xform(dir)
+	velocity = dir * SPEED * delta
+	move_and_slide()
