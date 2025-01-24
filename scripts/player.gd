0eec940de92a2442ddbf6624a40c81d566e5b5ad
Adam Sawicki <@>
2025-01-24 18:13:28 +0100

    Fixes to player input, incl. WSAD

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
-const SPEED := 3000.0
+const SPEED := 500.0
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector("Left", "Right", "Down", "Up")
-	print("dir=%s" % dir)
-	#var xformed_dir = transform.basis_xform(dir)
-	velocity = dir * SPEED * delta
+	var dir = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = dir * SPEED
 	move_and_slide()
