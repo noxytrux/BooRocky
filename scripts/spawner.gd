7aa7dd9aa74103024b77bab882622ab60c928f29
Krystian <szlagor.k@onet.pl>
2025-01-25 09:28:57 +0100

    Setup babies spawners

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -3,6 +3,11 @@ class_name Spawner extends ContainerItem
 @export var scene_to_load : PackedScene
 var createdItem : ItemBase
 @onready var spawn_item_timer: Timer = $SpawnItemTimer
+@export var spawn_time : float = 0.1
+
+func _ready() -> void:
+	_on_spawn_item_timer_timeout()
+	spawn_item_timer.wait_time = spawn_time
 
 func PlaceItem(item: ItemBase) -> void:
 	if(item == null):
@@ -13,6 +18,7 @@ func TakeItem() -> ItemBase:
 		return null
 	else:
 		var result = createdItem
+		ItemAnchor.remove_child(createdItem)
 		createdItem = null
 		spawn_item_timer.start()
 		return result
@@ -25,3 +31,5 @@ func CanTakeItem() -> bool:
 	
 func _on_spawn_item_timer_timeout() -> void:
 	createdItem = scene_to_load.instantiate()
+	ItemAnchor.add_child(createdItem)
+	createdItem.position = Vector2(0, 0)
