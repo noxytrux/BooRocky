381214e05e4576e1314397a956a1dbde3e350baa
Marcin Malysz <marcin.malysz@artifexmundi.com>
2025-01-25 19:35:29 +0100

    added sounds to menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,18 @@
 class_name MenuScene extends Node2D
 
+@onready var button_sound: AudioStreamPlayer2D = $button_sound
+
+func play_sound(sound: AudioStreamPlayer2D) -> void:
+	if not sound.playing:
+		sound.play()
+		
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_play_pressed() -> void:
+	play_sound(button_sound)
 	SingletonObject.switch_to_game()
 
 func _on_help_pressed() -> void:
+	play_sound(button_sound)
 	SingletonObject.switch_to_help()
