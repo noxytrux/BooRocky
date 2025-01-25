45da2eb4e9c0b3518cbedce5d558b86d25109b41
Marcin Malysz <marcin.malysz@artifexmundi.com>
2025-01-25 04:28:02 +0100

    Added missing enums

--- scripts/item_base.gd
+++ scripts/item_base.gd
@@ -3,10 +3,13 @@ class_name ItemBase extends Node2D
 enum ITEM_TYPE 
 {
 	PAMPERS, 
-	DIRTY_PAMPERS,
+	DIRTY_PAMPERS, #PAMPERS
 	FOOD,
 	DRUG,
-	BABY
+	BABY, #RESERVED
+	TOY, 
+	INGREDIENT, #FOOD
+	PART #TOY
 }
 
 @export var SelectedType = ITEM_TYPE.PAMPERS
