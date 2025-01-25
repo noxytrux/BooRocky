6845d705777170072112cf7d65b13afd6cf0e2f5
Adam Sawicki <@>
2025-01-25 13:01:33 +0100

    Added help screen

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,3 +5,6 @@ func _on_exit_pressed() -> void:
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+func _on_help_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/help.tscn")
