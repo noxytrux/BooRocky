26a4b15f294de1a1b57a1fe5b149f3516cf7d974
Krystian <szlagor.k@onet.pl>
2025-01-25 02:59:03 +0100

    Add logic to handle timer in spawner

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,14 +1,25 @@
 class_name Spawner extends ContainerItem
 
 @export var scene_to_load : PackedScene
+var createdItem : ItemBase
+@onready var spawn_item_timer: Timer = $SpawnItemTimer
 
 func PlaceItem(item: ItemBase) -> void:
 	if(item == null):
 		return;
 	
 func TakeItem() -> ItemBase:
-	var newItem = scene_to_load.instantiate()
-	return newItem
+	if (createdItem == null):
+		return null
+	else:
+		var result = createdItem
+		createdItem = null
+		spawn_item_timer.start()
+		return result
 
 func CanPlaceItem() -> bool:
 	return false
+
+func _on_spawn_item_timer_timeout() -> void:
+	createdItem = scene_to_load.instantiate()
+	print("end time")
