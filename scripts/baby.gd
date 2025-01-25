9bc316cd1d76a3b0661d1885b1feeb543d6b1082
Marcin Malysz <marcin.malysz@artifexmundi.com>
2025-01-25 01:24:44 +0100

    Initial baby logic

--- scripts/baby.gd
+++ scripts/baby.gd
@@ -1,14 +1,39 @@
 class_name Baby extends ItemBase
 
 @onready var body: AnimatedSprite2D = $Body
+@onready var need_timer: Timer = $need_timer
 
-bool canpickup = true
+var canpickup:bool = true
+var dead:bool = false
 
-]func _ready() -> void:
-	pass # Replace with function body.
+func _ready() -> void:
+	need_timer.start()
 
 func _process(delta: float) -> void:
-	pass
+	
+	global_scale = Vector2(1,1)
+	
+	if dead && !canpickup:
+		body.stop()
+		canpickup = true	
+	
 	
 func IsPickable() -> bool:
 	return canpickup
+
+func PickUp() -> void:
+	
+	if !dead:
+		canpickup = false
+	
+	body.play("bed")
+	
+func PutDown() -> void:
+	
+	if dead and canpickup:
+		dead = false
+		body.play("die")
+		canpickup = false
+
+func _on_need_timer_timeout() -> void:
+	dead = true
